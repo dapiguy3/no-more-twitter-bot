aae6fe37c5fd20231f404d08a70a8e66a86f1c44
pi <dapiguy3@gmail.com>
2025-02-05 19:49:39 -0600

    initial commit
    this bot is simple. i dont plan on changing it.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # no-more-twitter-bot
  discord bot built in python that removes twitter links.
+
+
+setup:
+install python 3
+add python 3 to path
+
+
+CMD commands:
+python -m pip install discord
+python -m pip install discord.py
+cd (whatever folder you put the bot)
+python main.py
+
+
+make sure to put your token inside the quotes in auth.py. dont share that token,
+i'm not responsible!
---
+++ main.py
@@ -0,0 +1,27 @@
+"""i'm keeping this bot super simple. documentation will remain minimal. good luck!"""
+
+#this local file contains your token. dont share it. dont stream it.
+#inb4 "use this encryption library you moron": shut it nerd, dont be an idiot!
+import auth 
+
+#imports specifically for discord api
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True
+
+#this is the actual bot object.
+bot = commands.Bot(intents=intents)
+
+@bot.event
+async def on_ready():
+    print("bot is live")
+
+
+@bot.event
+async def on_message(ctx):
+    if "x.com" in ctx.content.lower() or "twitter.com" in ctx.content.lower():
+        await ctx.delete()
+
+bot.run(auth.token)
